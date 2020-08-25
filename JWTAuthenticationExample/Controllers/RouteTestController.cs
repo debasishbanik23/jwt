e9bee4a09c9ddb15188f2d4cf0454e72218b609f
Debasish Banik <debasish.banik23@gmail.com>
2020-08-25 14:57:04 +0530

    d

---
+++ JWTAuthenticationExample/Controllers/RouteTestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[AllowAnonymous]
+    public class RouteTestController : ControllerBase
+    {
+        [HttpGet("v1")]
+        public IEnumerable<string> Get()
+        {
+            List<string> days = new List<string> { "Sunday", "Monday" };
+            return days;
+        }
+
+        [HttpGet("v2")]
+        public IEnumerable<string> Get2()
+        {
+            List<string> names = new List<string> { "Butul", "Banik" };
+            return names;
+        }
+
+        [HttpGet("{id:int:min(3)}")]
+        public string test1(int id)
+        {
+            return "ID= " + id;
+        }
+    }
+}
